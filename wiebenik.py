196b79d7fffa55ac0b0afcd09fdd3feae782b396
Paweł Kellner <karkuwa@Pawel-MacBook.local>
2020-09-16 13:17:21 +0200

    Difficulty keuze gemaakt

---
+++ wiebenik.py
@@ -0,0 +1,12 @@
+print("Welkom bij mijn Casino game :)")
+print("Wil je beginnen met spelen? \n A: Ja \n B: Nee")
+
+answer = input()
+
+if answer == "A":
+   print("Kies de moeilijkheid \n A: Easy \n B: Medium \n C: Hard")
+   difficulty = input()
+elif answer == "B":
+   print("Oké, doei!")
+
+print("XD")
